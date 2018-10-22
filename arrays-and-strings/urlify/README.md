9b4ff2840eab550f587858481caa2519d09d77a5
oi <oi@Ogors-MacBook-Pro.local>
2018-10-22 13:38:27 -0500

    tricky string problem

---
+++ arrays-and-strings/urlify/README.md
@@ -0,0 +1,36 @@
+URLify: Write a method to replace all spaces in a string with '%20'.
+You may assume that the string has sufficient space at the end to hold the
+additional characters, and that you are given the "true" length of the string.
+(NB: Please use a character array so that you can perform this operation in place.)
+
+EXAMPLE
+
+Input:  "Mr John Smith       ", 13
+Output: "Mr%20John%20Smith"
+
+Hints:
+
+    *   It is often easier to modify strings by going from the end to the beginning.
+
+    *   You might find you need to know the number of spaces. Can you just count them?
+
+SOLUTION
+
+    A common approach in string manipulation problems is to edit the string starting
+    from the end and working backwards. This is useful because we have an extra buffer at
+    the end, which allows us to change characters without worrying about what we 're
+    overwriting.
+
+    We will use this approach in this problem. The algorithm employs a two-scan approach.
+    *   In the first scan, we count the number of spaces. By tripling this number, we can
+        compute how many extra characters we will have in a final string.
+    *   In the second pass, which is done in reverse order, we actually edit the string.
+        when we see a space, we replace it with %20. If there is no space, then we copy
+        the original character.
+
+
+    NB: This solution is implemented using character arrays, because strings are immutable.
+    If we used strings directly, the function would have to return a new string, but it
+    would allow us to implement this in just one pass.
+
+    Please see URLify.java for implementation.
--- arrays-and-strings/urlify/URLify.java
+++ arrays-and-strings/urlify/URLify.java
@@ -1,63 +1,91 @@
 /**
+ * File: URLify.java
+ * -----------------
  * Question: Write a method to replace all spaces in a string with
  * '%20'. You may assume that the string has sufficient space at the
  * end to hold the additional characters, and that you are given the
  * "true" length of the string. (NB: Please use a character array so
  * that you can perform this operation in place.)
+ * --------------------------------------
+ * | Compilation: javac URLify.java     |
+ * | Execution:   java URLify.java      |
+ * --------------------------------------
  */
 public class URLify {
 
+    private static final String SENTINEL = "exit";
+    
     public static void main(String[] args) {
-        /*java.util.Scanner stdIn = new java.util.Scanner(System.in);
-        String input = stdIn.nextLine();
-        System.out.println(urlIfy(input));     
-        */
-        char[] word = {'M','r',' ','J','o','h','n',' ','S','m','i','t','h'};
-        replaceSpaces(word, 13);
-    }
-
-    /**
-     * Used to replace all spaces in a string with %.
-     * @pre Length of the string is known.
-     * @post replaces all spaces within the string with %20.<br>
-     * @param str The string to be examined for spaces.
-     * @param content A char array used to perform the replacement
-     *        operation in place.<br>
-     * @return A string with modified spaces as %20.
-     */
-    public static String urlIfy(String str) {
-        char[] content = str.toCharArray();
-        for (int i = 0; i < content.length; ++i) {
-            if (content[i] == ' ')
-                content[i] = '%';
+        /* introduce program  to user*/
+        greetUser();
+        /* prompt user for input */
+        while (true) {
+            String str = promptUser();
+            if (str.equalsIgnoreCase(SENTINEL)) break;
+            /* initialize a char array using str */
+            char[] word = str.toCharArray();
+            /* test solution 1 */
+            replaceSpaces1(word, word.length);
         }
-        return new String(content);
-        // return content.toString();
     }
 
-    /**
-     *Used to replace all spaces in a string with %20.
+
+    /**. Solution 1
+     * Used to replace all spaces in a string with %20.
+     * @post A transformed version of the input string.
+     * @param inputString Character array of string to be transformed.
+     * @param origLength Length of the character array.
      */
-    public static void replaceSpaces(char[] str, int trueLength) {
-        int spaceCount = 0, index, i = 0;
-        for (i = 0; i < trueLength; ++i) {
-            if (str[i] == ' ') {
-                spaceCount++;
+    private static void replaceSpaces1(char[] inputStr, int origLength) {
+        int numSpaces = 0, newIdx, i = 0; 
+        for (i = 0; i < origLength; i++) {
+            if (inputStr[i] == ' ') {
+                numSpaces++;
             }
         }
-        index = trueLength + spaceCount * 2;
-        if (trueLength < str.length) str[trueLength] = '\0'; // End array
-        for (i = trueLength - 1; i >= 0; i--) {
-            if (str[i] == ' ') {
-                str[index - 1] = '0';
-                str[index - 2] = '2';
-                str[index - 3] = '%';
-                index = index - 3;
-            } else {
-                str[index - 1] = str[i];
-                index--;
+        try {
+            newIdx = origLength + numSpaces * 2;
+            if (origLength < inputStr.length) inputStr[origLength] = '\0'; // End array
+            for (i = origLength - 1; i >= 0; i--) {
+                if (inputStr[i] == ' ') {
+                    inputStr[newIdx - 1] = '0';
+                    inputStr[newIdx - 2] = '2';
+                    inputStr[newIdx - 3] = '%';
+                    newIdx = newIdx - 3;
+                } else {
+                    inputStr[newIdx - 1] = inputStr[i];
+                    newIdx--;
+                }
             }
+        } catch (ArrayIndexOutOfBoundsException e) {
+               //e.printStackTrace();
         }
-
+        System.out.print(inputStr+"\n");
+        //System.out.print(java.util.Arrays.toString(inputStr)+"\n");
     }
+    /**.
+     * Time complexity:     O(N)
+     * Space complexity:    O(1)
+     * NB: You might experience issues with this algorithm if you are a vim user.
+     * Running on eclipse abstracts the issues.
+     */
+
+
+    /**.
+     * Used to prompt user for input
+     */
+    public static String promptUser() {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.print("Enter Sentence: ");
+        String str = sc.nextLine();
+        return str;
+    }
+
+    public static void greetUser() {
+        System.out.print("This program replaces the spaces in a string with \"%20\"\n");
+        System.out.print("To quit program, simply enter \"exit\" at prompt, thanks :)\n");
+        System.out.print("Please enter a sentence that you would like to URLify:\n");
+    }
+
+
 }
