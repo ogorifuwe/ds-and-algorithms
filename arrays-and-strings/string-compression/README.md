04f2d63ade4d0aaf331acbc4f5e0d94ba3266fad
oi <oi@Ogors-MacBook-Pro.local>
2018-10-24 15:42:48 -0500

    another string problem

---
+++ arrays-and-strings/string-compression/README.md
@@ -0,0 +1,45 @@
+String Compression:
+    Implement a method to perform basic string compression using the counts of repeated characters.
+    For example, the string aabcccccaaa would become a2b1c5a3. If the "compressed" string would not
+    become smaller than the original string, your method should return the original string. You can
+    assume the string has only uppercase and lowercase letters (a-z).
+
+    Hints:
+
+    *   Do the easy thing first. Compress the string, then compare the lengths.
+
+    *   Be careful that you aren't repeatedly concatenating strings together.
+        This can be very inefficient.
+
+    SOLUTION
+
+    1.  At first glance, implementing this method seems fairly straightforward, but perharps a tedious.
+        We iterate through the string, copying characters to a new string and counting the repeats. At
+        each iteration, check if the current character is the same as the next character. If not, add
+        its compressed version to the result.
+
+        Summary: This works. Is it efficient, though? Take a look at the runtime of this code.
+
+        The runtime is O(p + k*k), where p is the size of the original string and k is the number
+        of character sequences. For example, if the string is aabccdeeaa, then there are six
+        character sequences. It's slow because string concatenation operates in O(n*n) time.
+        We can fix this by using a StringBuilder.
+
+:
+    2. Using a string builder object.
+
+        Summary: Both of these solutions create the compressed string first and then return
+        the shorter of the input string and the compressed string.
+
+
+    3. We can instead, check in advance. This will be more optimal in cases where we don't
+       have a large number of repeating characters. It will avoid us having to create a string
+       that we never use. The downside of this is that it causes a second loop through the
+       characters and also adds a nearly duplicated code.
+
+       One other benefit of this approach is that we can initialize StringBuilder to its
+       necessary capacity up-front. Without this, StringBuilder will (behind the scenes)
+       need to double its capacity every time it hits capacity. The capacity could be what
+       we ultimately need.
+
+       See StringCompression.java for implementations
--- arrays-and-strings/string-compression/StringCompression.java
+++ arrays-and-strings/string-compression/StringCompression.java
@@ -1,73 +1,135 @@
-/**
- * Please refer to StringCompressionReadMe.md for details
- * of this class and its behaviors.
- * @author oi August 29, 2018
+/**.
+ * File: StringCompression.java
+ * ----------------------------
+ * This program compresses a string input by replace consecutive
+ * repeating characters with the character the count(number of times)
+ * of the character.
+ * Example:
+ * Input: "aaaabbbbbcccccddddddee"
+ * Output: "a4b5c5d6e2""
+ * ------------------------------------------------
+ * | Compliation: javac StringCompression.java    |
+ * | Execution:   java StringCompression          |
+ * ------------------------------------------------
+ *
+ * Please see README.md for details and performance of the algorithms
  */
 public class StringCompression {
     
     public static void main(String[] args) {
         /* entry point */
-        System.out.println(compressBad("aabcccccaaa"));
+        System.out.print(compressString1("abbcccddddeeeeeffffffeeeeeee")+"\n");
+        System.out.print(compressString1("abbcc")+"\n");
+        System.out.print(compressString2("abbcccddddeeeeeffffffeeeeeee")+"\n");
+        System.out.print(compressString2("abbcc")+"\n");
+        System.out.print(compressString3("abbcccddddeeeeeffffffeeeeeee")+"\n");
+        System.out.print(compressString3("abbcc")+"\n");
+
+
+
+
     }
 
-    /**
-     * Solution 1
-     * Naive algorithm used to compress a string. Please see 
-     * ReadMe for details.
+    /**. Solution 1
      * @param str The string to be compressed.
-     * @return The compressed version of str.
+     * @param csv Compressed String Version of str.
+     * @param ccc Consecutive Count of Character.
+     * @return The compressed string Version(csv).
      */
-    public static String compressBad(String str) {
-        String compressedString = "";
-        int countConsecutive = 0;
+    private static String compressString1(String str) {
+        String csv = "";
+        int ccc = 0;
         for (int i = 0; i < str.length(); ++i) {
-            countConsecutive++;
+            ccc++;
 
             /* if next character is different than
              * current, append this char to result */
-            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
-                compressedString += "" + str.charAt(i) + countConsecutive;
-                countConsecutive = 0;
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i+1)) {
+                csv += "" + str.charAt(i) + ccc;
+               ccc = 0;
             }
 
         }
-        return compressedString.length() < str.length() ? compressedString : str;
+        return csv.length() < str.length() ? csv : str;
     }
 
 
-    /**
-     * Solution 2
+    /**. Solutiopn 2
+     * This method uses a string builder object to append consecutive
+     * characters and count(for performance optimization reasons), 
+     * instead of string concatenation which has a complexity of O(n^2).
+     * Otherwise, algorithm, mechanisms and varaibles are as Solution 1.
      */
-    public static String compress(String str) {
-        StringBuilder sb = new StringBuilder();
-        int countConsecutive = 0;
+    private static String compressString2(String str) {
+        StringBuilder csv = new StringBuilder();
+        int ccc = 0;
         for (int i = 0; i < str.length(); ++i) {
-            countConsecutive++;
+            ccc++;
 
             /* if next character is different than current,
              * append this char to result */
-            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
-                compressed.append(str.charAt(i));
-                compressed.append(countConsecutive);
-                countConsecutive = 0;
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i+1)) {
+                csv.append(str.charAt(i));
+                csv.append(ccc);
+                ccc = 0;
             }
         }
-        return compressed.length() < str.length() ? compressed.toString() : str;
+        return csv.length() < str.length() ? csv.toString() : str;
     }
 
-    /**
-     * Solution 3
+    /**. Solution 3
+     * This method also uses a string builder object as in Solution,
+     * but with gets the capacity ahead of time which helps with
+     * tradeoffs of overhead cost for large enough string objects.
+     * See README for additional details.
+     * @param str The string to be compressed.
+     * @param csl compressed String Length.
+     * @param ccc Consecutive Count of Characters.
+     * @param csv Compressed String Version.
+     * @Return csv The Compressed Version of String.
      */
+    private static String compressString3(String str) {
+        int initialCapacity = countOfCSV(str);
+        if (initialCapacity >= str.length()) return str;
 
-    static int countCompression(String str) {
-        int compressionLength = 0;
-        int countConsecutive = 0;
+        StringBuilder csv = new StringBuilder(initialCapacity);
+        int ccc = 0;
         for (int i = 0; i < str.length(); ++i) {
-            countConsecutive++;
+            ccc++;
+            /* if next charcter is different than current,
+             * append this char to result */
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i+1)) {
+                csv.append(str.charAt(i));
+                csv.append(ccc);
+                ccc = 0;
+            }
+        }
+        return csv.toString();
+    } 
+
+    /**.
+     * This method is used to get the character count of the
+     * compressed string version of a string.
+     * @param str The string whose compressed verson length is to be
+     * determined.
+     * @param csl compressed String Length.
+     * @param ccc Consecutive Count of Characters.
+     * @return csl The length of the compressed string.
+     */
+    static int countOfCSV(String str) {
+        int csl = 0;
+        int ccc = 0;
+        for (int i = 0; i < str.length(); ++i) {
+            ccc++;
 
             /* if next character is different than current,
              * increase the length */
-            if (i + 1 >= str.length()  )
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i+1)) {
+                ccc++;
+                csl += 1 + String.valueOf(ccc).length();
+                ccc = 0;
+            }
         }
+        return csl;
     }
 }
