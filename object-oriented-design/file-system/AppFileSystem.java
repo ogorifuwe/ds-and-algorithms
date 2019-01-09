8ee6a4a5ebba82c6a325f88079041a2742e5fb8f
oi <oi@Ogors-MacBook-Pro.local>
2019-01-09 14:13:13 -0600

    file system

---
+++ object-oriented-design/file-system/AppFileSystem.java
@@ -0,0 +1,38 @@
+import file_system.*;
+
+public class AppFileSystem {
+
+    public static void main(String[] args) {
+    
+        Directory root = new Directory("Food", null);
+        File taco = new File("Taco", root, 4);
+        File hamburger = new File("Hamburger", root, 9);
+        root.addEntry(taco);
+        root.addEntry(hamburger);
+
+            Directory healthy = new Directory("Healthy", root);
+            
+                Directory fruits = new Directory("Fruits", healthy);
+                    File apple = new File("Apple", fruits, 5);
+                    File banana = new File("Banana", fruits, 6);
+                fruits.addEntry(apple);
+                fruits.addEntry(banana);
+
+            healthy.addEntry(fruits);
+
+                Directory veggies = new Directory("Veggies", healthy);
+                    File carrot = new File("Carrot", veggies, 6);
+                    File lettuce = new File("Lettuce", veggies, 7);
+                    File peas = new File("Peas", veggies, 4);
+                veggies.addEntry(carrot);
+                veggies.addEntry(lettuce);
+                veggies.addEntry(peas);
+
+            healthy.addEntry(veggies);
+
+        root.addEntry(healthy);
+
+        System.out.print(root.numberOfFiles()+"\n");
+        System.out.print(veggies.getFullPath()+"\n");
+    }
+}
---
+++ object-oriented-design/file-system/README.md
@@ -0,0 +1,12 @@
+File System: Explain the data structures and algorithm that you would
+use to design an in-memory file system. Illustrate with an example in
+code where possible.
+
+Hints:
+
+    *   This is not as complicated as it sounds. Start by making a list
+        of the key objects in the system, then think about how they in-
+        teract.
+    
+    *   What is the relationship between files and directories?
+
