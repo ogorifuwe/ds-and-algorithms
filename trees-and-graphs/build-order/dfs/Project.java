9eb8b63ac78a9ac48882dc083e8aedc83f0d21dc
oi <oi@Ogors-MacBook-Pro.local>
2019-01-09 01:07:01 -0600

    dfs

---
+++ trees-and-graphs/build-order/dfs/Project.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Project {
+
+    public enum State { COMPLETE, PARTIAL, BLANK};
+    private ArrayList<Project> children =
+                        new ArrayList<Project>();
+    private HashMap<String, Project> map =
+                        new HashMap<String, Project>();
+    private String name;
+    private State state = State.BLANK;
+
+    public Project(String name) { this.name = name; } 
+    
+    public String getName() { return name; }
+
+    public void addNeighbor(Project node) {
+        if (!map.containsKey(node.getName())) {
+            children.add(node);
+            map.put(node.getName(), node);
+        }
+    }
+
+    public State getState() { return state; }
+    public void setState(State state) { this.state = state; }
+
+    public ArrayList<Project> getChildren() { return children; }
+}
